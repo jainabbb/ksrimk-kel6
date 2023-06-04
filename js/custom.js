e4abe4846f2f0dede8705c5de934e8762b992a3b
jainabbb <nabilakm14@gmail.com>
2023-06-04 20:54:42 +0700

    kotak transparan

--- js/custom.js
+++ js/custom.js
@@ -91,3 +91,51 @@ $("select").on("change" , function() {
     
 });
 
+// // Get the modal
+// var modal = document.getElementById('myModal');
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById('myImg');
+// var modalImg = document.getElementById("img01");
+// img.onclick = function(){
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     modalImg.alt = this.alt;
+// }
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+function openModal() {
+	document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+	document.getElementById("myModal").style.display = "none";
+  }
+  
+  var slideIndex = 0;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+	showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+	showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+	var i;
+	var slides = document.querySelectorAll('.gallery__item img');
+	var captionText = document.getElementsByClassName("numbertext")[0];
+	var modalImg = document.querySelector('.modal-content img');
+	n %= slides.length;
+	modalImg.src = slides[n].src;
+	captionText.innerHTML = n + 1 + ' / ' + slides.length;
+  }

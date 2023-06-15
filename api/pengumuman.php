32f209c180c05e2937c77af17509b33c62c2ccf3
aljaffarsyah <aljaffarsyah10@gmail.com>
2023-06-15 13:58:31 +0700

    test

--- api/pengumuman.php
+++ api/pengumuman.php
@@ -1,3 +1,9 @@
+<?php
+if (!(session_status() === PHP_SESSION_ACTIVE)) {
+   session_start();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -40,157 +46,157 @@
    <?php
    $_SESSION['no_page'] = 4;
    include 'navbar.php'; ?>
-      <div class="banner_main">
-         <div class="news w3-animate-bottom" style="padding: 30px 20px;">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-12">
-                     <div class="titlepage" id="nonhome">
-                        <h2>Daftar Pengumuman</h2>
-                     </div>
+   <div class="banner_main">
+      <div class="news w3-animate-bottom" style="padding: 30px 20px;">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage" id="nonhome">
+                     <h2>Daftar Pengumuman</h2>
                   </div>
                </div>
-               <div class="opening daftarp">
-                  <div>
-                     <a href="konten pengumuman 1.php">
-                        <h2>Pengumuman Pasca Diklat - OPREC KSR 2022</h2>
-                        <h1>18-11-2022</h1>
-                     </a>
-                     <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 13!</p>
-                     <!-- <br> -->
-                     <p>Selamat sudah melewati Diklat 2 dan Tes Tulis 
-                        hari ini. Karena sebelumnya udah ada pengumuman pra diklat,
-                        berarti ada juga dong pemunguman pasca diklat. 
-                        Nah, mengenai apa aja yang perlu disiapkan setelah diklat, 
-                        berikut dilampirkan file pengumuman pasca diklatnya ya.
-                     </p><br>
-                     <p>Tetap jaga kesehatan dan semangatnya!!!</p>
-                     <br>
-                     <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
-                  </div>
-                  <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pasca_diklat.pdf" target=”_blank” class="pdf">
-                        <img src="../images/pdf.png" alt="unduh">
-                        <span><br></span>
-                        <span>pengumuman_pasca_diklat.pdf</span> <br>
-                        <span style="color: #c8ae79">Buka Dokumen</span>
+            </div>
+            <div class="opening daftarp">
+               <div>
+                  <a href="konten pengumuman 1.php">
+                     <h2>Pengumuman Pasca Diklat - OPREC KSR 2022</h2>
+                     <h1>18-11-2022</h1>
                   </a>
+                  <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 13!</p>
+                  <!-- <br> -->
+                  <p>Selamat sudah melewati Diklat 2 dan Tes Tulis
+                     hari ini. Karena sebelumnya udah ada pengumuman pra diklat,
+                     berarti ada juga dong pemunguman pasca diklat.
+                     Nah, mengenai apa aja yang perlu disiapkan setelah diklat,
+                     berikut dilampirkan file pengumuman pasca diklatnya ya.
+                  </p><br>
+                  <p>Tetap jaga kesehatan dan semangatnya!!!</p>
                   <br>
+                  <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
                </div>
-               <div class="opening daftarp">
-                  <div>
-                     <a href="konten pengumuman 2.php">
-                        <h2>Pengumuman Pra Diklat - OPREC KSR 2022</h2>
-                        <h1>13-11-2022</h1>
-                     </a>
-                     <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 13!</p>
-                     <!-- <br> -->
-                     <p>Bentar lagi Oprec Tahap 1 KSR Angkatan 13 akan dimulai. 
-                        Pastinya udah pada ga sabar dan semangat kan. 
-                        Nah, sebelum kita bertemu di diklat nanti, ada beberapa 
-                        ketentuan nih yang harus kalian perhatikan. 
-                        Ketentuannya ada di file terlampir yaa..
-                     </p>
-                     <p>Persiapkan semuanya dengan maksimal ya!</p>
-                     <br>
-                     <p>Tetap jaga kesehatan dan semangatnya !</p>
-                     <br>
-                     <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
-                  </div>
-                  <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumunan_pra_diklat.pdf" target=”_blank” class="pdf">
-                        <img src="../images/pdf.png" alt="unduh">
-                        <span><br></span>
-                        <span>pengumunan_pra_diklat.pdf</span> <br>
-                        <span style="color: #c8ae79">Buka Dokumen</span>
+               <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pasca_diklat.pdf" target=”_blank” class="pdf">
+                  <img src="../images/pdf.png" alt="unduh">
+                  <span><br></span>
+                  <span>pengumuman_pasca_diklat.pdf</span> <br>
+                  <span style="color: #c8ae79">Buka Dokumen</span>
+               </a>
+               <br>
+            </div>
+            <div class="opening daftarp">
+               <div>
+                  <a href="konten pengumuman 2.php">
+                     <h2>Pengumuman Pra Diklat - OPREC KSR 2022</h2>
+                     <h1>13-11-2022</h1>
                   </a>
+                  <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 13!</p>
+                  <!-- <br> -->
+                  <p>Bentar lagi Oprec Tahap 1 KSR Angkatan 13 akan dimulai.
+                     Pastinya udah pada ga sabar dan semangat kan.
+                     Nah, sebelum kita bertemu di diklat nanti, ada beberapa
+                     ketentuan nih yang harus kalian perhatikan.
+                     Ketentuannya ada di file terlampir yaa..
+                  </p>
+                  <p>Persiapkan semuanya dengan maksimal ya!</p>
                   <br>
+                  <p>Tetap jaga kesehatan dan semangatnya !</p>
+                  <br>
+                  <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
                </div>
-               <div class="opening daftarp">
-                  <div>
-                     <a href="konten pengumuman 3.php">
-                        <h2>Pengumuman Pasca Diklat - OPREC KSR 2021</h2>
-                        <h1>01-01-2022</h1>
-                     </a>
-                     <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 12!</p>
-                     <!-- <br> -->
-                     <p>Selamat yaa sudah melewati diklat hari ini, empat jempol buat kalian.</p>
-                     <p>Sebelumnya ada pengumuman pra diklat kan yaa, berarti selanjutnya ada...</p>
-                     <p>Ada pengumuman pasca diklat dong!</p>
-                     <br>
-                     <p>Berikut kami lampirkan file pengumuman pasca diklatnya yaa</p>
-                     <p>Persiapkan semuanya dengan sangat baik.</p>
-                     <br>
-                     <p>Tetap jaga kesehatan dan semangatnya</p>
-                     <br>
-                     <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
-                  </div>
-                  <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pasca_diklat.pdf" target=”_blank” class="pdf">
-                        <img src="../images/pdf.png" alt="unduh">
-                        <span><br></span>
-                        <span>pengumuman_pasca_diklat.pdf</span> <br>
-                        <span style="color: #c8ae79">Buka Dokumen</span>
+               <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumunan_pra_diklat.pdf" target=”_blank” class="pdf">
+                  <img src="../images/pdf.png" alt="unduh">
+                  <span><br></span>
+                  <span>pengumunan_pra_diklat.pdf</span> <br>
+                  <span style="color: #c8ae79">Buka Dokumen</span>
+               </a>
+               <br>
+            </div>
+            <div class="opening daftarp">
+               <div>
+                  <a href="konten pengumuman 3.php">
+                     <h2>Pengumuman Pasca Diklat - OPREC KSR 2021</h2>
+                     <h1>01-01-2022</h1>
                   </a>
+                  <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 12!</p>
+                  <!-- <br> -->
+                  <p>Selamat yaa sudah melewati diklat hari ini, empat jempol buat kalian.</p>
+                  <p>Sebelumnya ada pengumuman pra diklat kan yaa, berarti selanjutnya ada...</p>
+                  <p>Ada pengumuman pasca diklat dong!</p>
                   <br>
+                  <p>Berikut kami lampirkan file pengumuman pasca diklatnya yaa</p>
+                  <p>Persiapkan semuanya dengan sangat baik.</p>
+                  <br>
+                  <p>Tetap jaga kesehatan dan semangatnya</p>
+                  <br>
+                  <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
                </div>
-               <div class="opening daftarp">
-                  <div>
-                     <a href="konten pengumuman 4.php">
-                        <h2>Pengumuman Pra Diklat - OPREC KSR 2021</h2>
-                        <h1>01-01-2022</h1>
-                     </a>
-                     <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 12!</p>
-                     <!-- <br> -->
-                     <p>Selamat yaa telah melewati tes tertulis, gimana nih semangatnya masih tetap full kan yaa? </p>
-                     <p>Harus dong! Apalagi masih ada tahap selanjutnya, yap akan ada DIKLAT </p>
-                     <p>Nah, sebelum kita bertemu di diklat nanti, ada beberapa ketentuan nih yang harus kalian persiapkan. Ketentuannya ada di file terlampir yaa</p>
-                     <br>
-                     <p>Tetap jaga kesehatan dan semangatnya</p>
-                     <br>
-                     <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
-                  </div>
-                  <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pra_diklat.pdf" target=”_blank” class="pdf">
-                        <img src="../images/pdf.png" alt="unduh">
-                        <span><br></span>
-                        <span>pengumuman_pra_diklat.pdf</span> <br>
-                        <span style="color: #c8ae79">Buka Dokumen</span>
+               <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pasca_diklat.pdf" target=”_blank” class="pdf">
+                  <img src="../images/pdf.png" alt="unduh">
+                  <span><br></span>
+                  <span>pengumuman_pasca_diklat.pdf</span> <br>
+                  <span style="color: #c8ae79">Buka Dokumen</span>
+               </a>
+               <br>
+            </div>
+            <div class="opening daftarp">
+               <div>
+                  <a href="konten pengumuman 4.php">
+                     <h2>Pengumuman Pra Diklat - OPREC KSR 2021</h2>
+                     <h1>01-01-2022</h1>
                   </a>
+                  <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 12!</p>
+                  <!-- <br> -->
+                  <p>Selamat yaa telah melewati tes tertulis, gimana nih semangatnya masih tetap full kan yaa? </p>
+                  <p>Harus dong! Apalagi masih ada tahap selanjutnya, yap akan ada DIKLAT </p>
+                  <p>Nah, sebelum kita bertemu di diklat nanti, ada beberapa ketentuan nih yang harus kalian persiapkan. Ketentuannya ada di file terlampir yaa</p>
                   <br>
+                  <p>Tetap jaga kesehatan dan semangatnya</p>
+                  <br>
+                  <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
                </div>
-               <div class="opening daftarp">
-                  <div>
-                     <a href="konten pengumuman 5.php">
-                        <h2>Materi Tes Tertulis - OPREC KSR</h2>
-                        <h1>29-12-2021</h1>
-                     </a>
-                     <p>Tes Tertulis Kurang Tiga Hari Lagiiii</p>
-                     <p>Tenang gaiss, kami udah siapin nutrisi yang akan menemani kalian tiga hari ke depan supaya kalian siap menghadapi tes tertulis nanti</p>
-                     <p>Oke, cuss langsung diunduh dan dipelajari  materinya yaa!</p>
-                     <p>Good Luck, semoga diberi kemudahan dan kelancaran </p>
-                     <br>
-                     <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
-                  </div>
-                  <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/materi_oprec_angkatan_12.pdf" target=”_blank” class="pdf">
-                     <img src="../images/pdf.png" alt="unduh">
-                     <span><br></span>
-                     <span>materi_oprec_angkatan_12.pdf</span> <br>
-                     <span style="color: #c8ae79">Buka Dokumen</span>
+               <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pra_diklat.pdf" target=”_blank” class="pdf">
+                  <img src="../images/pdf.png" alt="unduh">
+                  <span><br></span>
+                  <span>pengumuman_pra_diklat.pdf</span> <br>
+                  <span style="color: #c8ae79">Buka Dokumen</span>
+               </a>
+               <br>
+            </div>
+            <div class="opening daftarp">
+               <div>
+                  <a href="konten pengumuman 5.php">
+                     <h2>Materi Tes Tertulis - OPREC KSR</h2>
+                     <h1>29-12-2021</h1>
                   </a>
+                  <p>Tes Tertulis Kurang Tiga Hari Lagiiii</p>
+                  <p>Tenang gaiss, kami udah siapin nutrisi yang akan menemani kalian tiga hari ke depan supaya kalian siap menghadapi tes tertulis nanti</p>
+                  <p>Oke, cuss langsung diunduh dan dipelajari materinya yaa!</p>
+                  <p>Good Luck, semoga diberi kemudahan dan kelancaran </p>
                   <br>
+                  <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
                </div>
-               <nav aria-label="Page navigation example">
-                  <ul class="pagination justify-content-end">
-                     <!-- <li class="page-item disabled">
+               <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/materi_oprec_angkatan_12.pdf" target=”_blank” class="pdf">
+                  <img src="../images/pdf.png" alt="unduh">
+                  <span><br></span>
+                  <span>materi_oprec_angkatan_12.pdf</span> <br>
+                  <span style="color: #c8ae79">Buka Dokumen</span>
+               </a>
+               <br>
+            </div>
+            <nav aria-label="Page navigation example">
+               <ul class="pagination justify-content-end">
+                  <!-- <li class="page-item disabled">
                         <a class="page-link" href="#" tabindex="-1">Previous</a>
                      </li> -->
-                     <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                     <li class="page-item"><a class="page-link" href="pengumuman2.php">2</a></li>
-                     <!-- <li class="page-item">
+                  <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="pengumuman2.php">2</a></li>
+                  <!-- <li class="page-item">
                         <a class="page-link" href="#">Next</a>
                      </li> -->
-                  </ul>
-               </nav>
-            </div>
+               </ul>
+            </nav>
          </div>
-         
+      </div>
+
       <div class="container">
          <div class="row">
          </div>

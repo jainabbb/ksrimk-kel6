2b4bb2fbb0dc0006925f81691e5f5baf3cd945ec
aljaffarsyah <aljaffarsyah10@gmail.com>
2023-06-17 23:22:41 +0700

    navbar search

--- api/search.php
+++ api/search.php
@@ -1,39 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-   <!-- basic -->
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <!-- mobile metas -->
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta name="viewport" content="initial-scale=1, maximum-scale=1">
-   <!-- site metas -->
-   <title>IMK Redesign Website KSR</title>
-   <meta name="keywords" content="">
-   <meta name="description" content="">
-   <meta name="author" content="">
-   <!-- bootstrap css -->
-   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
-   <link rel="stylesheet" href="../css/bootstrap.min.css">
-   <!-- style css -->
-   <link rel="stylesheet" href="../css/style.css">
-   <!-- Responsive-->
-   <link rel="stylesheet" href="../css/responsive.css">
-   <!-- fevicon -->
-   <link rel="icon" href="../images/fevicon.png" type="image/gif">
-   <!-- Scrollbar Custom CSS -->
-   <link rel="stylesheet" href="../css/jquery.mCustomScrollbar.min.css">
-   <!-- Tweaks for older IEs-->
-   <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-   <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
-   <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
-</head>
-<!-- body -->
+<?php include 'head.php' ?>
 
 <body class="main-layout">
    <!-- loader  -->
@@ -41,55 +6,9 @@
          <div class="loader"><img src="images/loading.gif" alt="#"></div>
       </div> -->
    <!-- end loader -->
-   <!-- header -->
-   <header>
-      <!-- header inner -->
-      <div class="header">
-         <div class="header_bottom d_flex">
-            <a class="logo" href="#"><img src="../images/logo.png" alt="#"></a>
-            <div class="container" style="margin-left: 10px;">
-               <div class="row">
-                  <div class="col-xl-8 col-lg-8 col-md-8 col-sm-8">
-                     <nav class="navigation navbar navbar-expand-md navbar-dark ">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                           <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarsExample04" style="height: 100%">
-                           <ul class="navbar-nav mr-auto">
-                              <li class="nav-item">
-                                 <a class="nav-link" href="index.php">Beranda</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="pengumuman.php">Pengumuman</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="artikel1.php">Artikel</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="galeri.php">Galeri</a>
-                              </li>
-                              <li class="nav-item">
-                                 <a class="nav-link" href="tentang.php">Tentang</a>
-                              </li>
-                           </ul>
-                        </div>
-                     </nav>
-                  </div>
-                  <div class="col-md-4">
-                     <div class="search">
-                        <form action="search.php" method="post">
-                           <input class="form_sea" type="text" placeholder="Cari artikel atau pengumuman" name="search">
-                           <button type="submit" class="seach_icon"><i class="fa fa-search"></i></button>
-                        </form>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-   </header>
-   <!-- end header inner -->
-   <!-- end header -->
+   <?php
+   $_SESSION['no_page'] = 1;
+   include 'navbar.php'; ?>
    <div class="banner_main">
       <!-- news section -->
       <div class="news w3-animate-bottom" style="padding: 30px 20px;">
@@ -256,46 +175,7 @@
    </div>
    <!-- end three_box section -->
 
-   <!--  footer -->
-   <footer>
-      <div class="footer">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-3">
-                  <div class="inror_box">
-                     <h3>Stay Updated!</h3>
-                  </div>
-               </div>
-            </div>
-            <div class="row footinfo">
-               <div class="col-md-6">
-                  <p>Email</p>
-                  <form class="news_form">
-                     <input class="letter_form" placeholder=" Masukkan Email Anda" type="text" name="Masukkan Email Anda">
-                     <br>
-                     <button class="submit">Subscribe</button>
-                  </form>
-               </div>
-               <div class="col-md-6">
-                  <img src="../images/location.png" alt="location" width="100" id="location">
-                  <span>Sekretariat KSR PMI Unit STIS Ruang PMKL, Gedung 3 Lantai 3</span><br>
-                  <span>Politeknik Statistika STIS</span><br>
-                  <span>Jalan Otto Iskandardinata 64C,</span><br>
-                  <span>Bidara Cina, Jatinegara, Jakarta Timur, DKI Jakarta, 13330</span>
-               </div>
-            </div>
-            <div class="socials">
-               <a href="mailto:ksrpmi@stis.ac.id"><i class="fa fa-solid fa-envelope"></i></a>
-               <a href="https://twitter.com/ksr_stis" target=”_blank”><i class="fa fa-twitter"></i></a>
-               <a href="https://www.instagram.com/ksr_stis/" target=”_blank”><i class="fa fa-instagram"></i></a>
-            </div>
-            <div class="socials">
-               <p>© Copyright 2023, Politeknik Statistika STIS</p>
-            </div>
-         </div>
-      </div>
-   </footer>
-   <!-- end footer -->
+   <?php include 'footer.php' ?>
    <!-- Javascript files-->
    <script src="../js/jquery.min.js"></script>
    <script src="../js/popper.min.js"></script>

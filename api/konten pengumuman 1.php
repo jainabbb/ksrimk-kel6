330df7ebd842fcff803b5111206ebd5f3b612ffc
jainabbb <nabilakm14@gmail.com>
2023-06-18 02:28:41 +0700

    coba scroll pdf mobile

--- api/konten pengumuman 1.php
+++ api/konten pengumuman 1.php
@@ -45,9 +45,9 @@
                </div>
             </a>
             <br>
-            <div style="overflow: auto!important; -webkit-overflow-scrolling: touch!important;">
-               <iframe id="pdf" src="https://jmp.sh/3Jk82iSP"  width="800" height="500"></iframe>
-            </div>
+            <object data="https://jmp.sh/3Jk82iSP" type="application/pdf" width="680" height="500">
+               <embed src="https://jmp.sh/3Jk82iSP" type="application/pdf" />
+            </object>
          </div>
          <div class="opening" id="daftarartikel" style="padding-left: 0; padding-right: 0; margin-bottom: 0;">
             <h2>Daftar Pengumuman</h2>

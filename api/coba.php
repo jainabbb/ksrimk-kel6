b2daaee559c25047f2fda6c31a3b764e6f8978fd
jainabbb <nabilakm14@gmail.com>
2023-06-18 15:06:51 +0700

    coba scroll pdf mobile

---
+++ api/coba.php
@@ -0,0 +1,98 @@
+<?php include 'head.php' ?>
+
+<body class="main-layout">
+   <!-- loader  -->
+   <!-- <div class="loader_bg">
+         <div class="loader"><img src="../images/loading.gif" alt="#"></div>
+      </div> -->
+   <?php $_SESSION['no_page'] = 4;
+   include 'navbar.php'; ?>
+   <!-- end loader -->
+   <!-- header -->
+   <div class="banner_main">
+      <!-- news section -->
+      <div class="news w3-animate-bottom" style="padding: 30px 20px;">
+         <div class="container" style="padding-left: 0; padding-right: 0;">
+            <div class="row">
+               <div class="col-md-12">
+                  <div class="titlepage" id="judulartikel">
+                     <h2>Pengumuman Pasca Diklat - OPREC KSR 2022</h2>
+                     <p>11-18-2022</p>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="grid-container">
+         <div id="kontenp">
+            <div>
+               <p style="font-weight: bold;">Halo Calon Anggota Muda KSR Angkatan 13!</p>
+               <!-- <br> -->
+               <p>Selamat sudah melewati Diklat 2 dan Tes Tulis
+                  hari ini. Karena sebelumnya udah ada pengumuman pra diklat,
+                  berarti ada juga dong pemunguman pasca diklat.
+                  Nah, mengenai apa aja yang perlu disiapkan setelah diklat,
+                  berikut dilampirkan file pengumuman pasca diklatnya ya.
+               </p><br>
+               <p>Tetap jaga kesehatan dan semangatnya!!!</p>
+               <br>
+               <p style="font-weight: bold;">Siamo Tutti Fratelli</p>
+            </div>
+            <br>
+            <a href="https://ksrpmistis.weebly.com/uploads/1/1/4/0/114042075/pengumuman_pasca_diklat.pdf" target=”_blank”>
+               <div class="pdf">
+                  <img src="../images/pdf.png" alt="unduh" style="margin-left: 0; padding-left: 0">
+                  <div class="juduldok pt-2">
+                     <span>pengumuman_pasca_diklat.pdf</span> <br>
+                     <span style="color: #c8ae79">Unduh Dokumen</span>
+                  </div>
+               </div>
+            </a>
+            <br>
+            <div class="iframe-container iframe-container-for-wxh-500x350" style="-webkit-overflow-scrolling: touch; overflow: auto;">
+            <iframe src="https://drive.google.com/file/d/1J8KUIINXb1w87q-X4opMM6BLSH_me3gq/preview"></iframe>
+            </div>
+         </div>
+         <div class="opening" id="daftarartikel" style="padding-left: 0; padding-right: 0; margin-bottom: 0;">
+            <h2>Daftar Pengumuman</h2>
+            <a href="konten pengumuman 1.php">
+               <p>Pengumuman Pasca Diklat - OPREC KSR 2022</p>
+            </a>
+            <a href="konten pengumuman 2.php">
+               <p>Pengumuman Pra Diklat - OPREC KSR 2022</p>
+            </a>
+            <a href="konten pengumuman 3.php">
+               <p>Pengumuman Pasca Diklat - OPREC KSR 2021</p>
+            </a>
+            <a href="konten pengumuman 4.php">
+               <p>Pengumuman Pra Diklat - OPREC KSR 2021</p>
+            </a>
+            <a href="konten pengumuman 5.php">
+               <p>Materi Tes Tertulis - OPREC KSR</p>
+            </a>
+            <a href="konten pengumuman 6.php">
+               <p>Pengumuman Pradiklat 3 - KSR Angkatan 11</p>
+            </a>
+            <a href="konten pengumuman 7.php">
+               <p>Pengumuman Pradiklat 2 - KSR Angkatan 11</p>
+            </a>
+            <a href="konten pengumuman 8.php">
+               <p>Pengumuman Pradiklat 1 - KSR Angkatan 11</p>
+            </a>
+            <a href="konten pengumuman 9.php">
+               <p>Pengumuman Pasca Diklat Calon Anggota KSR Angkatan 11</p>
+            </a>
+            <a href="pengumuman.php">
+               <p>Pengumuman lainnya...</p>
+            </a>
+         </div>
+         </div>
+
+      </div>
+      <div class="container">
+         <div class="row">
+         </div>
+      </div>
+   </div>
+   <!-- end three_box section -->
+
+   <?php include 'footer.php' ?>
